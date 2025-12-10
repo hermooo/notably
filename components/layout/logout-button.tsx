bfb665032900c0885f2c1145651c7264becb02b6
devxyn <johnlawrencehermo@gmail.com>
2025-12-11 02:58:16 +0800

    feat: implemented logout feature

--- components/layout/logout-button.tsx
+++ components/layout/logout-button.tsx
@@ -3,12 +3,23 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { Loader2 } from "lucide-react";
+import { logoutUser } from "@/actions/auth.action";
+import { toast } from "sonner";
 
 export default function LogoutButton() {
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleLogout = () => {
-    console.log("Logging out...");
+  const handleLogout = async () => {
+    setLoading(true);
+    const { error, message } = await logoutUser();
+
+    if (error) {
+      toast.error(message);
+    } else {
+      toast.success(message);
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -18,7 +29,7 @@ export default function LogoutButton() {
       disabled={loading}
       onClick={handleLogout}
     >
-      {loading ? <Loader2 className="animate-spin" /> : "Logout"}
+      {loading ? <Loader2 className="h-4 w-4 animate-spin" /> : ""} Logout
     </Button>
   );
 }
