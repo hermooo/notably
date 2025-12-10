ab0f7efa43d4d9bda2f969821bbf2a76a32a3269
devxyn <johnlawrencehermo@gmail.com>
2025-12-11 02:57:41 +0800

    feat: implemented signup  feature in signup form

--- app/(auth)/signup/signup-form.tsx
+++ app/(auth)/signup/signup-form.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { signupUser } from "@/actions/auth.action";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -7,23 +10,75 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    startTransition(async () => {
+      const { name, email, password, confirmPassword } = formData;
+
+      if (password !== confirmPassword) {
+        toast.error("Passwords do not match");
+        return;
+      }
+
+      const { error, message } = await signupUser(name, email, password);
+
+      if (error) {
+        toast.error(message);
+        return;
+      }
+
+      toast.success(message);
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+      router.push("/login");
+    });
+  };
+
   return (
     <Card {...props}>
       <CardHeader>
         <CardTitle>Create an account</CardTitle>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmitCapture={handleSubmit}>
           <FieldGroup className="gap-4">
             <Field className="gap-1">
               <FieldLabel htmlFor="name">Full Name</FieldLabel>
               <Input
                 id="name"
+                name="name"
                 type="text"
                 placeholder="Enter your name"
+                value={formData.name}
+                onChange={(e) => handleInputChange(e)}
                 required
               />
             </Field>
@@ -31,8 +86,11 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
               <FieldLabel htmlFor="email">Email</FieldLabel>
               <Input
                 id="email"
+                name="email"
                 type="email"
                 placeholder="Enter your email"
+                value={formData.email}
+                onChange={(e) => handleInputChange(e)}
                 required
               />
             </Field>
@@ -40,8 +98,11 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
               <FieldLabel htmlFor="password">Password</FieldLabel>
               <Input
                 id="password"
+                name="password"
                 type="password"
                 placeholder="Enter your password"
+                value={formData.password}
+                onChange={(e) => handleInputChange(e)}
                 required
               />
               <FieldDescription className="text-muted-foreground text-xs">
@@ -55,7 +116,10 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
               <Input
                 id="confirm-password"
                 type="password"
+                name="confirmPassword"
                 placeholder="Enter your password again"
+                value={formData.confirmPassword}
+                onChange={(e) => handleInputChange(e)}
                 required
               />
               <FieldDescription className="text-muted-foreground text-xs">
@@ -64,7 +128,18 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
             </Field>
             <FieldGroup>
               <Field>
-                <Button type="submit">Create Account</Button>
+                <Button
+                  type="submit"
+                  disabled={isPending}
+                  className="cursor-pointer"
+                >
+                  {isPending ? (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  ) : (
+                    ""
+                  )}
+                  Create Account
+                </Button>
                 <FieldDescription className="px-6 text-center">
                   Already have an account? <Link href="/login">Sign in</Link>
                 </FieldDescription>
